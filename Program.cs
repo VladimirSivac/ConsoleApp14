e61e26c28d615f59687855d5d0fac208b9cea615
VladimirSivac <morndes@gmail.com>
2024-12-23 17:44:24 +0200

    Task-3 done

--- Program.cs
+++ Program.cs
@@ -10,7 +10,24 @@ namespace ConsoleApp14
     {
         static void Main(string[] args)
         {
-           
+           Hello();
+        }
+
+        static void Hello()
+        {
+            // Цикл с параметром
+            /*for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Привет!");
+            }*/
+            
+            // Цикл с условием
+            int n = 1;
+            while (n <= 5)
+            {
+                Console.WriteLine("Привет!");
+                n++;
+            }
         }
     }
 }
