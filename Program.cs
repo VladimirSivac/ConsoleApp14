57a40418000eac69486b7a99d0ab85a1b38ea024
VladimirSivac <morndes@gmail.com>
2024-12-23 17:39:48 +0200

    Task-2 done

--- Program.cs
+++ Program.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp14
 {
+    /// <summary>
+    /// Вывести на экран целые числа из интервала от 1 до n (цикл с параметром)
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            int n = Input("Введите n: ");
+            Func(n);
+        }
+
+        static int Input(string message)
+        {
+            Console.Write(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        static void Func(int n)
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }   
         }
     }
 }
