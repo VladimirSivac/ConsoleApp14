55d9155f7d024515bd1e74510fd6d1fd517fba69
VladimirSivac <morndes@gmail.com>
2024-12-23 17:34:42 +0200

    task-1 done

--- Program.cs
+++ Program.cs
@@ -6,11 +6,35 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp14
 {
+    /// <summary>
+    /// Вывести на экран в столбик первые 10 натуральных чисел (циклы с условием, цикл с параметром)
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
-           
+            int n = 1;
+            
+            // Цикл с предусловием
+            /*while (n <= 10)
+            {
+                Console.WriteLine(n);
+                n++;
+            }*/
+            
+            // Цикл с постусловием
+            /*do
+            {
+                Console.WriteLine(n);
+                n++;
+            } while (n <= 10);*/
+            
+            // Цикл с параметром
+            /*for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }*/
+            
         }
     }
 }
