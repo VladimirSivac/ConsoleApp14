1832fa4c9c150c09b94f250b91b77d1275399a2b
VladimirSivac <morndes@gmail.com>
2024-12-23 18:07:46 +0200

    Task-4 done

--- Program.cs
+++ Program.cs
@@ -6,11 +6,39 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp14
 {
+    /// <summary>
+    /// С использованием цветового оформления консоли: а) вывести на экран горизонтальную строку из 18 символов; б) вывести на экран вертикальную строку из 25 символов.
+    /// </summary>
     internal class Program
     {
         static void Main(string[] args)
         {
+           // Настройки консоли
+           Console.Title = "Вывод горизонтальной и вертикальной строк";
+           Console.BackgroundColor = ConsoleColor.Cyan;
+           Console.ForegroundColor = ConsoleColor.Red;
            
+           // Вывод строк
+           HorizontalLine();
+           VerticalLine();
+        }
+
+        static void HorizontalLine()
+        {
+            string line = "x";
+            for (int i = 1; i <= 18; i++)
+            {
+                Console.Write(line);
+            }
+        }
+        
+        static void VerticalLine()
+        {
+            string line = "x";
+            for (int i = 1; i <= 25; i++)
+            {
+                Console.WriteLine(line);
+            }
         }
     }
 }
